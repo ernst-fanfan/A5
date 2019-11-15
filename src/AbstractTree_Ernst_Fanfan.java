319ced72192fc0742139115301fc229380fa4e31
Ernst Fanfan <ernst.r.fanfan@gmail.com>
2019-11-15 00:24:26 -0500

    Added all given code for BST

--- src/AbstractTree_Ernst_Fanfan.java
+++ src/AbstractTree_Ernst_Fanfan.java
@@ -1,2 +1,28 @@
-public class AbstractTree_Ernst_Fanfan {
+//Class: CS 5040
+//Term: Fall 2019
+//Name: Ernst Fanfan
+//Instructor: Dr. Haddad
+//Assignment: 5
+//IDE Name: IntelliJ
+public abstract class AbstractTree_Ernst_Fanfan<E> implements Tree_Ernst_Fanfan<E> {
+    @Override
+    /** Inorder traversal from the root*/
+    public void inorder() {
+    }
+
+    @Override
+    /** Postorder traversal from the root */
+    public void postorder() {
+    }
+
+    @Override
+    /** Preorder traversal from the root */
+    public void preorder() {
+    }
+
+    @Override
+    /** Return true if the tree is empty */
+    public boolean isEmpty() {
+        return getSize() == 0;
+    }
 }
