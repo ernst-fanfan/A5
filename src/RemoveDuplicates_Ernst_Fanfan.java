e747099296c538ef277b09a53008a6a3339099f0
Ernst Fanfan <ernst.r.fanfan@gmail.com>
2019-11-16 11:47:48 -0500

    Added main menu loop added processing skeleton

--- src/RemoveDuplicates_Ernst_Fanfan.java
+++ src/RemoveDuplicates_Ernst_Fanfan.java
@@ -1,2 +1,31 @@
 public class RemoveDuplicates_Ernst_Fanfan {
+    public static void main (String [] Args){
+        boolean exit = false;//exit trigger
+
+        /**Start of program*/
+        while(exit != true) {//main loop
+            Menu_Ernst_Fanfan start = new Menu_Ernst_Fanfan();//new menu object
+            int choice = start.setChoice();//Scan and pass choice
+            exit = process(choice);//process choice and query exit
+        }
+    }
+
+    public static boolean process(int choice){
+        boolean exit = false;//exit trigger
+
+        /**switch between choices*/
+        switch (choice){
+            case 0://exit
+                System.out.println("Goodye!");
+                exit = true;//set to exit
+                break;
+            case 1://from file
+                System.out.println("Processing from file!");
+                break;
+            case 2://from user
+                System.out.println("Processing from User!");
+                break;
+        }
+        return exit;//return exit trigger
+    }
 }
