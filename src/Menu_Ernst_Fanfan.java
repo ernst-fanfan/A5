b110591bfe96d0c966c3e4f78abcff2e53374d12
Ernst Fanfan <ernst.r.fanfan@gmail.com>
2019-11-16 12:58:27 -0500

    Added the menus for User input and File manipulation

--- src/Menu_Ernst_Fanfan.java
+++ src/Menu_Ernst_Fanfan.java
@@ -5,20 +5,39 @@ public class Menu_Ernst_Fanfan {
     //states
     private int choice =-1;
 
-    public void startMenue(){
+    public void startMenu(){
         System.out.println("-----MAIN MENU-----\n" +
                 "0 - Exit Program\n" +
                 "1 – Read Input From Text File\n" +
                 "2 – Read Input Form Keyboard");
     }
 
+    public void fromUserMenu(){
+        System.out.println("-----USER MENU-----\n" +
+                "0 - Main Menu\n" +
+                "1 – Add Node\n" +
+                "2 - Remove last Node");
+    }
+
+    public void fromFileMenu(){
+        System.out.println("-----FILE MENU-----\n" +
+                "0 - Main Menu\n" +
+                "1 – Display file content\n" +
+                "2 - Process File");
+    }
+
     /**updating choice from user*/
-    public int setChoice(){
+    public int setChoice(int menu){
         Scanner intInput =  new Scanner(System.in);//scanner
         boolean continueLoop = true;//exit loop trigger
         do {//data validation loop
             try{//exception handling
-                startMenue();//call start menu
+                if (menu == 0)
+                    startMenu();//call start menu
+                else if (menu == 1)
+                    fromUserMenu();//call from user menu
+                else
+                    fromFileMenu();//call from file menu
                 choice = intInput.nextInt();//set choice to input
                 if (choice < 0 || choice > 2)
                     System.out.println("Try again. Invalid input.\nPlease enter 0, 1, or 2");//error message
