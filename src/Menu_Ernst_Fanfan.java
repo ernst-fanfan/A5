922942e16253fe0f4bc1ff89cdf431feeeaf421f
Ernst Fanfan <ernst.r.fanfan@gmail.com>
2019-11-22 23:24:56 -0500

    Added removed complicated menu objects and streamlined menu logic

--- src/Menu_Ernst_Fanfan.java
+++ src/Menu_Ernst_Fanfan.java
@@ -12,42 +12,14 @@ public class Menu_Ernst_Fanfan {
                 "2 – Read Input Form Keyboard");
     }
 
-    public void fromUserMenu(){
-        System.out.println("\n \n-----USER MENU-----\n" +
-                "0 - Main Menu\n" +
-                "1 – Add Node\n" +
-                "2 - Remove Duplicates");
-    }
-
-    public void fromFileMenu(){
-        System.out.println("\n \n-----FILE MENU-----\n" +
-                "0 - Main Menu\n" +
-                "1 – Load and Display file content\n" +
-                "2 - Process File");
-    }
-
-    public void addNodeMenu(){
-        System.out.println("\n \n-----ADD NODE MENU-----\n" +
-                "0 - User Menu\n" +
-                "1 – Add Node\n"+
-                "2 - Remove Last Node");
-    }
 
     /**updating choice from user*/
-    public int setChoice(int menu){
+    public int setChoice(){
         Scanner intInput =  new Scanner(System.in);//scanner
         boolean continueLoop = true;//exit loop trigger
         do {//data validation loop
+            startMenu();//call start menu
             try{//exception handling
-                if (menu == 0)
-                    startMenu();//call start menu
-                else if (menu == 1)
-                    fromUserMenu();//call from user menu
-                else if (menu == 2)
-                    fromFileMenu();//call from file menu
-                else
-                    addNodeMenu();//call from add node menu
-
                 choice = intInput.nextInt();//set choice to input
                 if (choice < 0 || choice > 2)
                     System.out.println("Try again. Invalid input.\nPlease enter 0, 1, or 2");//error message
